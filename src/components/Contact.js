e32f8f08e7f45f02bf465c42a9e91d7cdf226c70
Fede Escobar <fescobar_2@hotmail.com>
2021-08-17 16:10:52 -0400

    Added onSubmit to Netlify Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact() {
   const [name, setName] = React.useState("");
-  const [email, setEmail] =React.useState("");
+  const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
   
   function encode(data) {
@@ -10,7 +10,7 @@ export default function Contact() {
       .map(
         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
       )
-      .join("&")      
+      .join("&");      
   }
 
   function handleSubmit(e) {
@@ -20,7 +20,7 @@ export default function Contact() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
-    .then(() => alert("Message Sent!"))
+    .then(() => alert("Message sent!"))
     .catch((error) => alert(error));
   }
 
@@ -66,6 +66,7 @@ export default function Contact() {
         <form
           netlify
           name="contact"
+          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
@@ -108,7 +109,7 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
